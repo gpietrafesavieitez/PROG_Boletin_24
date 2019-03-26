c34a22be385e7608e844429dfea011a3b556cf97
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-03-26 12:14:57 +0100

    init

---
+++ Boletin_24_1/src/boletin_24_1/Boletin_24_1.java
@@ -0,0 +1,13 @@
+/**
+ * Gabriel P.
+ * CPR Daniel Castelao
+ * @author <gpietrafesavieitez@danielcastelao.org>
+ */
+
+package boletin_24_1;
+
+public class Boletin_24_1 {
+
+    public static void main(String[] args) {
+    }
+}
